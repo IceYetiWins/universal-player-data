d9d9131be2d1d4966e4b09f100dbc8c13472a139
Ice Yeti <101294194+IceYetiWins@users.noreply.github.com>
2025-03-04 20:10:02 -0500

    at universal player data spawn when join world

--- src/main/java/com/iceyetiwins/universalPlayerData/UniversalPlayerData.java
+++ src/main/java/com/iceyetiwins/universalPlayerData/UniversalPlayerData.java
@@ -1,10 +1,24 @@
 package com.iceyetiwins.universalPlayerData;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.TeleportTarget;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UniversalPlayerData implements ModInitializer {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger("universalPlayerData");
+
     @Override
     public void onInitialize() {
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            ServerPlayerEntity player = handler.getPlayer();
+
+            player.teleportTo(player.getRespawnTarget(true, TeleportTarget.NO_OP));
+
+            LOGGER.info("{} logged in at ({}, {}, {})", player.getName().getString(), player.getX(), player.getY(), player.getZ());
+        });
     }
 }
--- src/main/java/com/iceyetiwins/universalPlayerData/mixin/PlayerManagerMixin.java
+++ src/main/java/com/iceyetiwins/universalPlayerData/mixin/PlayerManagerMixin.java
@@ -4,9 +4,11 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.world.PlayerSaveHandler;
+import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Optional;
@@ -23,6 +25,21 @@ public abstract class PlayerManagerMixin {
 
             cir.setReturnValue(optional);
         }
+    }
+
+    @Redirect(
+            method = "onPlayerConnect",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lorg/slf4j/Logger;info(Ljava/lang/String;[Ljava/lang/Object;)V"
+            )
+    )
+    private void suppressLoginLog(Logger logger, String message, Object... objects) {
+        String playerName = (String) objects[0];
+        String playerIp = (String) objects[1];
+        int playerId = (int) objects[2];
+
+        logger.info("{}[{}] logged in with entity id {}", playerName, playerIp, playerId); //remove location from log because it will differ depending on spawn point
     }
 }
 
