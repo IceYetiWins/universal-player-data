44574e35a6d2a4cccf9d8f953e7177f7ab9c1d6d
Ice Yeti <101294194+IceYetiWins@users.noreply.github.com>
2026-03-30 17:55:43 -0400

    feature parity yay

--- src/main/java/com/iceyetiwins/universalPlayerData/UniversalPlayerData.java
+++ src/main/java/com/iceyetiwins/universalPlayerData/UniversalPlayerData.java
@@ -1,25 +1,26 @@
 package com.iceyetiwins.universalPlayerData;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.portal.TeleportTransition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class UniversalPlayerData implements ModInitializer {
-    public static final String MOD_ID = "universal-player-data-2";
-
-    // This logger is used to write text to the console and the log file.
-    // It is considered best practice to use your mod id as the logger's name.
-    // That way, it's clear which mod wrote info, warnings, and errors.
+    public static final String MOD_ID = "universal-player-data";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public static final String[] NBT_TAGS = {"abilities", "LastDeathLocation", "playerGameType", "respawn", "warden_spawn_tracker"};
 
     @Override
     public void onInitialize() {
-        // This code runs as soon as Minecraft is in a mod-load-ready state.
-        // However, some things (like resources) may still be uninitialized.
-        // Proceed with mild caution.
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            ServerPlayer player = handler.getPlayer();
 
-        LOGGER.info("this work?");
+            player.teleport(player.findRespawnPositionAndUseSpawnBlock(false, TeleportTransition.DO_NOTHING));
+
+            LOGGER.info("{} logged in at ({}, {}, {})", player.getName().getString(), player.getX(), player.getY(), player.getZ());
+        });
     }
 }
--- src/main/java/com/iceyetiwins/universalPlayerData/mixin/PlayerListMixin.java
+++ src/main/java/com/iceyetiwins/universalPlayerData/mixin/PlayerListMixin.java
@@ -1,5 +1,6 @@
 package com.iceyetiwins.universalPlayerData.mixin;
 
+import com.iceyetiwins.universalPlayerData.UniversalPlayerData;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.players.NameAndId;
 import net.minecraft.server.players.PlayerList;
@@ -9,6 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Optional;
@@ -19,9 +21,20 @@ public class PlayerListMixin {
     @Shadow
     private PlayerDataStorage playerIo;
 
-    //this method tries to load world data instead of the player dat file, we cannot have this
+    //this method tries to load world data instead of the player dat file when in singleplayer, we cannot have this
     @Inject(at = @At("RETURN"), method = "loadPlayerData", cancellable = true)
     private void forceLoadUniversalPlayerData(NameAndId nameAndId, CallbackInfoReturnable<Optional<CompoundTag>> cir) {
         cir.setReturnValue(playerIo.load(nameAndId));
+    }
+
+    @Redirect(
+            method = "placeNewPlayer",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lorg/slf4j/Logger;info(Ljava/lang/String;[Ljava/lang/Object;)V"
+            )
+    )
+    private void removeCoordinatesFromLog(org.slf4j.Logger logger, String message, Object[] args) {
+        UniversalPlayerData.LOGGER.info("{}[{}] logged in with entity id {}", args[0], args[1], args[2]);
     }
 }
